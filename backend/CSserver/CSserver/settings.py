96759b1cca0dfe5c10b4049ed5fa678550bc1957
Changsu Park <gd12091@gmail.com>
2023-10-19 19:42:12 +0900

    !FIX : Security Issue

--- backend/CSserver/CSserver/settings.py
+++ backend/CSserver/CSserver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure---#o7krrb9eajnf&0k^*yg9o^-5x2!17w2ffvuvyu$+f+ltnhm'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
